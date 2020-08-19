43d4314fb2ae11efb715dd8c2568c45f3a0d424e
Danielle Monahan <dmonahan@udel.edu>
2020-08-19 17:20:45 -0400

    Add humidity to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -16,7 +16,7 @@ const forecast = (latitude, longitude, callback) => {
             // });
             var printSentence = body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature+
             " degrees out but it feels like " 
-            + body.current.feelslike + " degrees"
+            + body.current.feelslike + " degrees. The humidity is " + body.current.humidity + "%."
            
             callback(undefined, printSentence);
        
